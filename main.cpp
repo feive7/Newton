86eda99752f14a68024bb02ed9939f733cfcc42a
Roland Baker <feive7@gmail.com>
2026-02-27 13:33:22 -0800

    basic ball ground

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char** argv) {
 
 	// Create viewport
 	Camera2D viewport;
-	viewport.offset = { 400,450 };
+	viewport.offset = { 400,225 };
 	viewport.rotation = 180.0f;
 	viewport.target = { 0,0 };
 	viewport.zoom = 10.0f;
@@ -34,24 +34,18 @@ int main(int argc, char** argv) {
 	// Create ground
 	BoxBody ground({0,0},{10,1},true);
 
-	// Create box tower
-	BoxBody boxes[10];
-	for(int i = 0; i < 10; i++) {
-		float y = 2.1*float(i)+2;
-		boxes[i] = BoxBody({0,y},{1,1},false);
-	}
-
-	// Create wrecking ball
-	EmptyBody constraint({0,25});
-	BallBody wrecking_ball({-10,25},3.0f,false);
-	DistanceJoint line(&constraint,&wrecking_ball,15);
+	// Create ball
+	BallBody ball({0,10},2.0f,false);
 
 	// Main loop
 	while (!WindowShouldClose()) {
 		if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
 			Vector2 mouse_position = GetScreenToWorld2D(GetMousePosition(), viewport);
-			Vector2 force_direction = mouse_position - wrecking_ball.getPos();
-			wrecking_ball.setVelocity(force_direction * 10);
+			Vector2 force_direction = mouse_position - ball.getPos();
+			ball.setVelocity(force_direction * 10);
+		}
+		if (IsKeyPressed(KEY_SPACE)) {
+			ball.destroy();
 		}
 
 		PhysicsStep(timestep, substep_count);
@@ -60,11 +54,7 @@ int main(int argc, char** argv) {
 		ClearBackground(RAYWHITE);
 		BeginMode2D(viewport);
 		ground.draw();
-		wrecking_ball.draw();
-		for(int i = 0; i < 10; i++) {
-			boxes[i].draw();
-		}
-		DrawJoint(line.id);
+		ball.draw();
 		EndMode2D();
 		EndDrawing();
 	}
